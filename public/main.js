68682f3ed1fe451811f643efdae8be966708a3bc
Ayrton Miotti <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-02 00:40:44 -0300

    Pagina-Web_Gary-Goodsped
    Students page.

--- public/main.js
+++ public/main.js
@@ -3,11 +3,15 @@ window.onload = function(){
       //welcome();
   }, 2000)
   $(".animation1").delay(2000).fadeOut();
-  $("body").removeClass(".animation1");
-  $("body").removeClass(".animation2"); //Fijarse bien
+  $("body").removeClass(".animation1"); //Fijarse bien
 }
 
+const califi = document.querySelector('#Btn#1');
+const matter = document.querySelector('#Btn#2');
 
+matter.addEventListener('click', e =>{
+  $("#ShSec1").delay(100).fadeOut();
+})
 
 const btn = document.querySelector("#menu-btn");
 const menu = document.querySelector("#sidemenu");
@@ -16,10 +20,3 @@ btn.addEventListener("click", e =>{
   menu.classList.toggle("menu-collapsed");
   document.querySelector("body").classList.toggle("body-expanded");
 });
-
-
-// Prueba branch - Ejemplo Faq
-// Yeh eh eh eh, puto el que diga que si en ingleh YES YES YES YES, es tu cumple me re regale yeh eh eh eh
-// Mi azucar tiene un poco de café yeh eh eh eh no calculé XD
-// En mi cajón quiero que le peguen figus de Dragon Ball y de Pocoyó
-// Yeh eh eh eh, no me corto las uñas de los pies Yeh eh eh eh por eso calzo 46

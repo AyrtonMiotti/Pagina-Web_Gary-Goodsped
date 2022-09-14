477b88f4b3b4d6a58f03e6f1ed97f4cd54bccb03
martinaaaaaaaaa22 <103609836+AyrtonMiotti@users.noreply.github.com>
2022-09-14 09:49:26 -0300

    Pagina-Web_Gary-Goodsped
    Massive changes.

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "",
     database: "gary_goodsped",
 })
 
